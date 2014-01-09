907b4835687a3909bb58bf97d55e0f2818264426
Meng Zhuo <mengzhuo1203@gmail.com>
2014-01-09 23:20:06 +0800

    alter vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 if [ ! -z "$JENKINS_HOME" ]
 then
   DEVICE_NAME=${LUNCH#*_} DEVICE_NAME=${DEVICE_NAME%-*}
-  if [ "$DEVICE_NAME" == "u8825" ]
+  if [ "$DEVICE_NAME" == "u8825d" ]
   then
     sh device/huawei/msm7x27a-common/patches/apply.sh
     sh device/huawei/u8833/patches/apply.sh
